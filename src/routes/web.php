1c5d3c3895831224bb7bfeba23e7a6bb32fd905b
emiyokoyama <emieminokuni728@gmail.com>
2025-03-03 16:00:26 +0900

    ルート定義の追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -18,6 +18,6 @@ Route::get('/products', [ProductController::class, 'index'])->name('products.ind
 Route::post('/products', [ProductController::class, 'store'])->name('products.store');
 Route::get('/products/{productId}', [ProductController::class, 'show'])->name('products.show');
 Route::patch('/products/{productId}/update', [ProductController::class, 'update'])->name('products.update');
-Route::get('/products/register',[ProductController::class, 'create']);
+Route::get('/products/register',[ProductController::class, 'create'])->name('products.create');
 Route::get('/products/search', [ProductController::class, 'search'])->name('products.search');
 Route::delete('/products/{productId}', [ProductController::class, 'destroy']);
